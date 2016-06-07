417378c768a4a22e96a402eb3943aae918e4b1b4
Daniel Martinez <daniel@kartjuba.com>
2016-06-07 03:40:51 -0300

    Added sort alphabetically

--- DMOrionTest/OrionTest/DMMainScreenVC.swift
+++ DMOrionTest/OrionTest/DMMainScreenVC.swift
@@ -11,13 +11,16 @@ import PKHUD
 import SnapKit
 import CoreLocation
 
+
 class DMMainScreenVC: UIViewController {
   
   var contacts: Array<DMContact>?
+  let tableView: UITableView = UITableView()
+  var orderAlphabeticallyReversed: Bool = false;
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor.lightGrayColor()
     initializeUI()
     HUD.show(.LabeledProgress(title: "Loading Contacts", subtitle: "Please wait..."))
     loadTestdata()
@@ -26,30 +29,87 @@ class DMMainScreenVC: UIViewController {
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
+  
   private func loadTestdata() {
+    
     DMTestController.sharedInstance.retrieveAndParseContacts { (contacts, error) in
       HUD.hide()
       self.contacts = contacts
+      
+      dispatch_async(dispatch_get_main_queue(), { 
+        self.tableView.reloadData()
+      })
+      
     }
   }
   
 }
 
+
 extension DMMainScreenVC {
   
   func initializeUI() {
     
-    self.title = "Contacts"
+    view.backgroundColor = UIColor.lightGrayColor()
     
-    let table = UITableView()
-    self.view.addSubview(table)
+    title = "Contacts"
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("A...Z", comment:""),
+          style: UIBarButtonItemStyle.Plain, target: self, action: #selector(onOrderPressed))
+    
+    let table = self.tableView
+    table.tableFooterView = UIView()
+    table.dataSource = self
+    view.addSubview(table)
     table.snp_makeConstraints { (make) in
       make.edges.equalTo(self.view)
     }
     
+  }
+  
+  func onOrderPressed() {
+    orderAlphabeticallyReversed = !self.orderAlphabeticallyReversed
+    navigationItem.rightBarButtonItem?.title = orderAlphabeticallyReversed ? "A...Z" : "Z...A"
+    tableView.reloadData()
+  }
+  
+}
+
+
+extension DMMainScreenVC : UITableViewDataSource {
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    if let count = self.contacts?.count {
+      return count
+    } else {
+      return 0
+    }
+
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCellWithIdentifier("contactCellId")
+    if cell == nil {
+      cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "contactCellId")
+    }
+    
+    if let contacts = orderedContacts(orderAlphabeticallyReversed) {
+      let contact = contacts[indexPath.row]
+      cell!.textLabel?.text = contact.name
+      cell!.detailTextLabel?.text = contact.email
+    }
+    
+    return cell!
+  }
+  
+  func orderedContacts(reverse:Bool) -> Array<DMContact>? {
+    if let contacts = self.contacts {
+      return reverse ? contacts.sort({$0.name > $1.name}) : contacts.sort({$0.name < $1.name})
+    }
+    return nil
   }
   
 }
